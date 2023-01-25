7de13ff0aa58f1931c96cd9ee47b70e6ca16594c
saerom <sagesaeromkim@gmail.com>
2023-01-25 13:09:31 +0900

    전체 사이즈, 스타일 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import './App.css';
 import Header from './components/Header/Header';
 import TodoList from './components/TodoList/TodoList';
 import DarkModeProvider from './context/DarkModeContext';
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,21 +6,23 @@ import { useDarkMode } from '../../context/DarkModeContext';
 export default function Header({ filters, filter, onFilterChange }) {
     const { darkMode, toggleDarkMode } = useDarkMode();
     return (
+        <>
         <header className={styles.header}>
-            <h1>To Do List</h1>
-            <ul className={styles.filters}>
+            <button className={styles.toggle} onClick={()  => toggleDarkMode()}>
+                {!darkMode ? <BsMoonFill /> : <BsSunFill /> }
+            </button>
+            <div className={styles.navbar}>
+                <h2>To Do List</h2>
+                <ul className={styles.filters}>
                 {filters.map((value, index) => (
                     <li key={index} >
                         <button className={`${styles.filter} ${filter === value && styles.selected}`} 
                         onClick={() => onFilterChange(value)}>{value}</button>
                     </li>))}
-            </ul>
-
-            <button className={styles.toggle} onClick={() => toggleDarkMode()}>
-                {!darkMode ? <BsMoonFill /> : <BsSunFill /> }
-            </button>
-
+                </ul>  
+            </div>
         </header>
+        </>
     );
 }
 
