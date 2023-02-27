96fe4ec9026bcab49249967de0cc9c977d98115b
saerom <sagesaeromkim@gmail.com>
2023-02-27 23:04:58 +0900

    readme 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import { useState } from 'react';
-import Header from './components/Header/Header';
-import TodoList from './components/TodoList/TodoList';
-import DarkModeProvider from './context/DarkModeContext';
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import TodoList from "./components/TodoList/TodoList";
+import DarkModeProvider from "./context/DarkModeContext";
 
-const filters = ['all', 'active', 'done'];
+const filters = ["all", "active", "done"];
 
-function App() {
+function App(): JSX.Element {
   const [filter, setFilter] = useState(filters[0]);
   return (
     <DarkModeProvider>
-      <Header 
-        filters={filters} 
-        filter={filter} 
-        onFilterChange={setFilter}/>
-      <TodoList 
-        filter={filter}/>
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </DarkModeProvider>
   );
 }
